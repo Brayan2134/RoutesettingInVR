a26b75964606c97a5377f21587659e19b933b3af
Brayan Quevedo Ramos <brayanquevedo2134@gmail.com>
2024-05-17 23:35:06 -0700

    Initial Commit

---
+++ HelloARWorld/HelloARWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloARWorldApp.swift
+//  HelloARWorld
+//
+//  Created by Brayan Quevedo Ramos on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloARWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
